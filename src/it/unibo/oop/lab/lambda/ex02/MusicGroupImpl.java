dbd2ca09a1059b0b461cde9648f6f46b4ca9f084
Luca Lucioli <luca.lucioli@studio.unibo.it>
2021-12-04 17:37:12 +0100

    implemented countSongsInNoAlbum method

--- src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
+++ src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
@@ -51,7 +51,7 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public int countSongsInNoAlbum() {
-        return -1;
+        return (int) this.songs.stream().filter(s -> s.albumName.equals(Optional.empty())).count();
     }
 
     @Override
