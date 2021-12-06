b5c6eddb7372d307f18d0a73b3b6895c1e640804
Luca Lucioli <luca.lucioli@studio.unibo.it>
2021-12-06 19:00:39 +0100

    implemented longestAlbum method

--- src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
+++ src/it/unibo/oop/lab/lambda/ex02/MusicGroupImpl.java
@@ -1,11 +1,15 @@
 package it.unibo.oop.lab.lambda.ex02;
 
+import java.security.KeyStore.Entry;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Optional;
 import java.util.OptionalDouble;
 import java.util.Set;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
@@ -88,7 +92,15 @@ public final class MusicGroupImpl implements MusicGroup {
 
     @Override
     public Optional<String> longestAlbum() {
-        return null;
+        return songs.
+        stream()
+        .filter(s -> s.getAlbumName().isPresent())
+        .collect(Collectors.groupingBy(Song::getAlbumName, Collectors.summingDouble(Song::getDuration)))
+        .entrySet()
+        .stream()
+        .collect(Collectors.maxBy(Comparator.comparingDouble(Map.Entry::getValue)))
+        .get()
+        .getKey();
     }
 
     private static final class Song {
